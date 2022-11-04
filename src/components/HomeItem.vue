45f6276025336148333ae28789555ab5b09ffdd8
aryman <amandaryman@gmail.com>
2022-11-04 12:17:07 -0700

    CowpokeLounge with auth0

---
+++ src/components/HomeItem.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-card class="ma-6">
+		<v-card-title>
+			{{ title }}
+		</v-card-title>
+
+		<v-card-text>
+			<v-row align="center" class="px-2" hide-gutters no-gutters>
+				<v-col cols="8">
+					{{ text }}
+				</v-col>
+				<v-col align="right">
+					<v-btn v-if="link != ''" class="ma-2 color--bold" :color="color" :icon="icon" size="x-large" :href="link"></v-btn>
+					<v-btn v-else class="ma-2 color--bold" :color="color" :icon="icon" size="x-large" :to="route"></v-btn>
+				</v-col>
+			</v-row>
+		</v-card-text>
+	</v-card>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	title: String,
+	text: String,
+	icon: String,
+	color: String,
+	route: Object,
+	link: String
+})
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+	<PageLayout>
+		<div class="app-title">
+			<h1>Welcome to the Cowpoke Lounge</h1>
+		</div>
+		<div>
+			The world's foremost virtual resource for aficionados of all things cattle-tending related
+		</div>
+		<HomeItem v-for="(item, index) in items" :title="item.title" :text="item.text" :icon="item.icon"
+			:color="item.color" :route="item.route" :link="item.link" :key="item.id" />
+	</PageLayout>
+</template>
+
+<script lang="ts">
+import HomeItem from '@/components/HomeItem.vue'
+import PageLayout from '@/components/navigation/PageLayout.vue'
+
+export default {
+	name: "Home",
+	components: { HomeItem, PageLayout },
+	data() {
+		return {
+			items: [
+				{
+					id: 1,
+					title: 'Get Started',
+					text: 'Create an account for a personalized experience',
+					icon: 'mdi-arrow-right-circle',
+					color: 'info',
+					route: { name: 'login' },
+					link: ''
+				},
+				{
+					id: 2,
+					title: 'Stay Connected',
+					text: 'Check out The Feed to stay up to date on all things cowpoke-related!',
+					icon: 'mdi-newspaper-variant-multiple',
+					color: 'warning',
+					route: { name: 'feed' },
+					link: ''
+				},
+				{
+					id: 3,
+					title: 'Why Cowpoke Lounge?',
+					text: 'Because it\'s silly fun! Find more silliness over on GitHub',
+					icon: 'mdi-github',
+					color: 'secondary',
+					route: undefined,
+					link: 'https://github.com/adnammit'
+				},
+			]
+		}
+	},
+}
+</script>
